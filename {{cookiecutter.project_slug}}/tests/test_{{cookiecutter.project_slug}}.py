1a9f555df42645ffd5284005231cf9fbfe19e60e
Marco Gancitano <marco.gancitano97@gmail.com>
2020-12-19 13:38:35 -0500

    Make CI compliant from the start

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,1 +1,10 @@
 """Test module for {{cookiecutter.project_slug}}."""
+
+from {{cookiecutter.project_slug}} import __author__, __email__, __version__
+
+
+def test_project_info():
+    """Test __author__ value."""
+    assert __author__ == "{{cookiecutter.full_name}}"
+    assert __email__ == "{{cookiecutter.email}}"
+    assert __version__ == "0.0.0"
