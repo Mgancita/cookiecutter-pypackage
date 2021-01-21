df07a1012b539716f86901b8f304412a8b5e3031
Marco Gancitano <marco.gancitano97@gmail.com>
2021-01-21 16:00:06 -0500

    Fix mypy error

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -8,6 +8,6 @@ __email__ = "{{ cookiecutter.email }}"
 # Used to automatically set version number from github actions
 # as well as not break when being tested locally
 try:
-    __version__ = version(__package__)
+    __version__ = version(__package__)  # type: ignore
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "0.0.0"
